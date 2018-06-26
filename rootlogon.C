e3eb8db2dda6151dac7b4b63982aeab02e7cb9e6
Paul Seyfert <pseyfert.mathphys@gmail.com>
2018-06-26 15:16:52 +0200

    make xrange available in root

--- rootlogon.C
+++ rootlogon.C
@@ -1,5 +1,7 @@
 {
-  TString incpath(Form("%s/.local/include",gSystem->Getenv("HOME")));
-  gSystem->AddIncludePath(Form("-I%s",incpath.Data()));
+  TString incpath(Form("%s/.local/include", gSystem->Getenv("HOME")));
+  gSystem->AddIncludePath(Form("-I%s", incpath.Data()));
   gInterpreter->AddIncludePath(incpath.Data());
+  gROOT->ProcessLine("#include \"range/v3/all.hpp\"");
+  gROOT->ProcessLine("auto xrange = ranges::view::indices;");
 }
