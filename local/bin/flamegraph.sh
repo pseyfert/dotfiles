4f6fa04fa8f7db0f408869f578b748bd6fef34b8
Paul Seyfert <paul.seyfert@sevensense.ch>
2020-09-29 14:41:50 +0200

    vtune flamegraph helper

---
+++ local/bin/flamegraph.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+THISDIR=$(readlink -f ${(%):-%N})
+FLAMEGRAPHDIR=${THISDIR:h:h:h}/submodules/FlameGraph
+file=$(mktemp --suffix=.csv)
+vtune -R top-down -call-stack-mode all -column="CPU Time:Self","Module" -report-out ${file}.org -filter "Function Stack" -format csv -csv-delimiter comma -r $1
+tail +2 ${file}.org > ${file}
+perl ${FLAMEGRAPHDIR}/stackcollapse-vtune.pl ${file}  | perl ${FLAMEGRAPHDIR}/flamegraph.pl > ${1%vtune}svg
+rm ${file}.org
+rm ${file}
